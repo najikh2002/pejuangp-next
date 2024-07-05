4fe848fb3ecdd397a1b8e0855f7e7e536e4c92a5
“najikh2002” <hizbullahnajihan@gmail.com>
2024-07-05 16:03:07 +0700

    fix: reafctory header code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,15 @@
 import Link from "next/link";
 import MainNav from "./ui/main-nav";
 import MobileNav from "./ui/mobile-nav";
+import LogoHeader from "./ui/logo";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between py-6 md:py-10 mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 dark:text-white">
-      <div className="">
-        <Link href="/">
-          <h2>HIZZDEV</h2>
-        </Link>
-      </div>
+      <LogoHeader />
 
       <MainNav />
+
       <MobileNav />
     </header>
   );
