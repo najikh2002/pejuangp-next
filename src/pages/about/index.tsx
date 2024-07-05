88d7550524b77b3fe72a11d622985b5f55d8bca6
“najikh2002” <hizbullahnajihan@gmail.com>
2024-07-06 05:46:51 +0700

    add: content & layout about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,6 +4,35 @@ export default function About() {
   return (
     <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <TitlePage title="About" />
+
+      <div className="py-6 min-h-[75vh]">
+        <p>
+          Kenalin, kami Pejuang Pemrograman. <br />
+          <br />
+          Jadi gini, kami ini komunitas anak-anak tech yang suka ngoprek kode
+          dan teknologi. Tapi tenang, kamu nggak harus jago dulu buat gabung.
+          Malah, kami percaya siapa aja bisa belajar pemrograman. <br />
+          <br />
+          Di sini, nggak ada yang namanya "pertanyaan bodoh". Mau tanya cara
+          bikin "Hello World" atau diskusi tentang AI terbaru, semuanya welcome!
+          Prinsip kami simpel: semua orang sama di sini. <br />
+          <br />
+          Kita semua punya sesuatu buat dibagiin, kan? Entah itu pengalaman
+          debug sampai pagi atau trik coding yang bikin hidup lebih mudah. Nah,
+          di Pejuang Pemrograman, kita saling berbagi dan belajar bareng. <br />
+          <br />
+          Intinya, kita pengen bikin komunitas yang asik buat belajar teknologi.
+          Tempat di mana kamu bisa nanya apa aja tanpa takut dibilang noob, dan
+          di mana kamu juga bisa pamer dikit kalau berhasil nyelesaiin proyek
+          keren. <br />
+          <br />
+          Jadi, mau ikutan jadi pejuang? Yuk, gabung! Bareng-bareng kita
+          taklukkan dunia teknologi, satu baris kode at a time. <br />
+          <br />
+          Pejuang Pemrograman: Di mana kita semua jadi guru sekaligus murid.
+          Seru kan?
+        </p>
+      </div>
     </div>
   );
 }
