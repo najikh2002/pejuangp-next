2eed0f5b75a9ac8329d5c4938121b43718b9df74
“najikh2002” <hizbullahnajihan@gmail.com>
2024-07-05 16:04:26 +0700

    add: constant for link & title nav

---
+++ src/lib/constant.ts
@@ -0,0 +1,19 @@
+export type NavLink = {
+  title: string;
+  href: string;
+};
+
+export const navLink: NavLink[] = [
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Project",
+    href: "/project",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+];
