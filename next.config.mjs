deb74c2a2215c005991c8293befca1d829e8797e
“najikh2002” <hizbullahnajihan@gmail.com>
2024-07-07 15:26:25 +0700

    fix: config external link image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com', 'www.notion.so', 'avatars.githubusercontent.com'],
+},
 };
 
 export default nextConfig;
