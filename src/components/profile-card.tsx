bcff8b23da9e348ea9b51555e117e3977bb7e0a2
“najikh2002” <hizbullahnajihan@gmail.com>
2024-09-04 17:35:05 +0700

    add: disable author profile

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function ProfileCard({ className }: { className?: string }) {
   return (
     <dl className={`justify-center items-center gap-2 py-2 ${className}`}>
-      <Image
+      {/* <Image
         className="w-10 h-10 rounded-full border-[1px] border-gray-400"
         src="https://avatars.githubusercontent.com/u/98723436?v=4"
         alt="avatar"
@@ -12,7 +12,7 @@ export default function ProfileCard({ className }: { className?: string }) {
         height={40}
       />
       <div className="whitespace-nowrap text-sm font-medium leading-5 py-1">
-        {/* <dt className="sr-only">Authors</dt>
+        <dt className="sr-only">Authors</dt>
         <dd className="text-gray-900 dark:text-gray-100">Hizbullah Najihan</dd>
         <dt className="sr-only">Instagram</dt>
         <dd className="flex">
@@ -24,8 +24,8 @@ export default function ProfileCard({ className }: { className?: string }) {
           >
             @hizz.nn
           </Link>
-        </dd> */}
-      </div>
+        </dd>
+      </div> */}
     </dl>
   );
 }
