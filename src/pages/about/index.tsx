5078d43f0730d5068ef4d4849a0bf9ce125a44d6
“najikh2002” <hizbullahnajihan@gmail.com>
2024-09-16 05:40:09 +0700

    add: meta head about & project pages

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,39 +1,66 @@
 import TitlePage from "@/components/title-page";
+import Head from "next/head";
 
 export default function About() {
   return (
-    <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
-      <TitlePage title="About" />
+    <>
+      <Head>
+        <title>About</title>
+        <meta
+          name={"description"}
+          title={"description"}
+          content={"Apa itu Pejuang Pemrograman?"}
+        />
+        <meta
+          name={"og:title"}
+          title={"og:title"}
+          content={"About - Pejuang Pemrograman"}
+        />
+        <meta
+          name={"og:description"}
+          title={"og:description"}
+          content={"Apa itu Pejuang Pemrograman?"}
+        />
+        <meta
+          name={"og:image"}
+          title={"og:image"}
+          content={"https://najikh2002.github.io/assets/pejuangp/cover.png"}
+        />
+      </Head>
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+        <TitlePage title="About" />
 
-      <div className="py-6 min-h-[75vh]">
-        <p>
-          Kenalin, kami Pejuang Pemrograman. <br />
-          <br />
-          Jadi gini, kami ini komunitas anak-anak tech yang suka ngoprek kode
-          dan teknologi. Tapi tenang, kamu nggak harus jago dulu buat gabung.
-          Malah, kami percaya siapa aja bisa belajar pemrograman. <br />
-          <br />
-          Di sini, nggak ada yang namanya &quot;pertanyaan bodoh&quot;. Mau
-          tanya cara bikin &quot;Hello World&quot; atau diskusi tentang AI
-          terbaru, semuanya welcome! Prinsip kami simpel: semua orang sama di
-          sini. <br />
-          <br />
-          Kita semua punya sesuatu buat dibagiin, kan? Entah itu pengalaman
-          debug sampai pagi atau trik coding yang bikin hidup lebih mudah. Nah,
-          di Pejuang Pemrograman, kita saling berbagi dan belajar bareng. <br />
-          <br />
-          Intinya, kita pengen bikin komunitas yang asik buat belajar teknologi.
-          Tempat di mana kamu bisa nanya apa aja tanpa takut dibilang noob, dan
-          di mana kamu juga bisa pamer dikit kalau berhasil nyelesaiin proyek
-          keren. <br />
-          <br />
-          Jadi, mau ikutan jadi pejuang? Yuk, gabung! Bareng-bareng kita
-          taklukkan dunia teknologi, satu baris kode at a time. <br />
-          <br />
-          Pejuang Pemrograman: Di mana kita semua jadi guru sekaligus murid.
-          Seru kan?
-        </p>
+        <div className="py-6 min-h-[75vh]">
+          <p>
+            Kenalin, kami Pejuang Pemrograman. <br />
+            <br />
+            Jadi gini, kami ini komunitas anak-anak tech yang suka ngoprek kode
+            dan teknologi. Tapi tenang, kamu nggak harus jago dulu buat gabung.
+            Malah, kami percaya siapa aja bisa belajar pemrograman. <br />
+            <br />
+            Di sini, nggak ada yang namanya &quot;pertanyaan bodoh&quot;. Mau
+            tanya cara bikin &quot;Hello World&quot; atau diskusi tentang AI
+            terbaru, semuanya welcome! Prinsip kami simpel: semua orang sama di
+            sini. <br />
+            <br />
+            Kita semua punya sesuatu buat dibagiin, kan? Entah itu pengalaman
+            debug sampai pagi atau trik coding yang bikin hidup lebih mudah.
+            Nah, di Pejuang Pemrograman, kita saling berbagi dan belajar bareng.{" "}
+            <br />
+            <br />
+            Intinya, kita pengen bikin komunitas yang asik buat belajar
+            teknologi. Tempat di mana kamu bisa nanya apa aja tanpa takut
+            dibilang noob, dan di mana kamu juga bisa pamer dikit kalau berhasil
+            nyelesaiin proyek keren. <br />
+            <br />
+            Jadi, mau ikutan jadi pejuang? Yuk, gabung! Bareng-bareng kita
+            taklukkan dunia teknologi, satu baris kode at a time. <br />
+            <br />
+            Pejuang Pemrograman: Di mana kita semua jadi guru sekaligus murid.
+            Seru kan?
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -2,6 +2,7 @@ import TitlePage from "@/components/title-page";
 import { NotionService } from "@/services/notion-service";
 import { ProjectPost } from "@/types/schema";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -28,55 +29,80 @@ export default function Project({
   allPosts,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
-      <TitlePage
-        title="Project"
-        description="List project Pejuang Pemrograman member :)"
-      />
-      <ul className="min-h-[75vh] py-6 flex flex-col md:grid md:grid-cols-2 gap-6">
-        {allPosts.map((post: ProjectPost) => (
-          <Link
-            href={`${post.link}`}
-            className="transition duration-300 hover:scale-105"
-            key={post.id}
-            target="_blank"
-          >
-            <div
-              key={post.title}
-              className="flex flex-col rounded-xl shadow-lg overflow-hidden bg-white dark:bg-gray-800"
+    <>
+      <Head>
+        <title>Project</title>
+        <meta
+          name={"description"}
+          title={"description"}
+          content={"List project anggota Pejuang Pemrograman :)"}
+        />
+        <meta
+          name={"og:title"}
+          title={"og:title"}
+          content={"Project - Pejuang Pemrograman"}
+        />
+        <meta
+          name={"og:description"}
+          title={"og:description"}
+          content={"List project anggota Pejuang Pemrograman :)"}
+        />
+        <meta
+          name={"og:image"}
+          title={"og:image"}
+          content={"https://najikh2002.github.io/assets/pejuangp/cover.png"}
+        />
+      </Head>
+      <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+        <TitlePage
+          title="Project"
+          description="List project Pejuang Pemrograman member :)"
+        />
+        <ul className="min-h-[75vh] py-6 flex flex-col md:grid md:grid-cols-2 gap-6">
+          {allPosts.map((post: ProjectPost) => (
+            <Link
+              href={`${post.link}`}
+              className="transition duration-300 hover:scale-105"
+              key={post.id}
+              target="_blank"
             >
-              <div className="flex-shrink-0">
-                <Image
-                  className="h-64 w-full object-fit"
-                  src={`${post.cover?.url}`}
-                  alt={post.title}
-                  height={256}
-                  width={500}
-                />
-              </div>
-              <div className="flex-1 bg-gray-50 dark:bg-gray-900 pt-2 pb-6 px-4 flex flex-col justify-between">
-                <div className="flex-1">
-                  <span className="block mt-2">
-                    <h4 className="text-xs font-medium text-gray-600 dark:text-gray-400">
-                      {post.date}
-                    </h4>
-                  </span>
-                  <span className="block mt-2">
-                    <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                      {post.title}
-                    </h3>
-                  </span>
-                  <span className="block mt-2">
-                    <p className="text-sm text-gray-600 dark:text-gray-400">
-                      By {post.author}
-                    </p>
-                  </span>
+              <div
+                key={post.title}
+                className="flex flex-col rounded-xl shadow-lg overflow-hidden bg-white dark:bg-gray-800"
+              >
+                <div className="flex-shrink-0">
+                  <Image
+                    className="h-64 w-full object-fit"
+                    src={`${post.cover?.url}`}
+                    alt={post.title}
+                    height={256}
+                    width={500}
+                  />
+                </div>
+                <div className="flex-1 bg-gray-50 dark:bg-gray-900 pt-2 pb-6 px-4 flex flex-col justify-between">
+                  <div className="flex-1">
+                    <span className="block mt-2">
+                      <h4 className="text-xs font-medium text-gray-600 dark:text-gray-400">
+                        {post.date}
+                      </h4>
+                    </span>
+                    <span className="block mt-2">
+                      <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                        {post.title}
+                      </h3>
+                    </span>
+                    <span className="block mt-2">
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        By {post.author}
+                      </p>
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      </ul>
-    </section>
+            </Link>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
